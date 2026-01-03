b8df2121c5b014435cf360f277eff4a120c0da42
Besix2 <myusernamethis@outlook.de>
2026-01-03 01:15:42 +0100

    added cahing for links

--- import re.py
+++ import re.py
@@ -1,6 +1,8 @@
-import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 import os
+import random
+import time
+import json
 
 
 class kleinanzeigen_monitor:
@@ -9,14 +11,30 @@ class kleinanzeigen_monitor:
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
         self.link = link
-        self.seen_ids = set()
         self.cookie_accepted = False
         self.first_start = True
-        #initialize cache------------------------------------------------
-        self.cache_name = str(self.link).split("/")[-2]
+        # initialize cache------------------------------------------------
+        self.cache_name =f"{str(self.link).split("/")[-2]}.json"
         self.id_folder = "cached_ids"
         self.full_cache_path = os.path.join(self.id_folder, self.cache_name)
         os.makedirs(self.id_folder, exist_ok=True)
+        self.cache = {}
+        #load cache
+        self.scanned_offers = self.load_cache()
+
+    def load_cache(self):
+        #create new empty id cache
+        if not os.path.exists(self.full_cache_path):
+            print(f"Erstelle neue Datenbank: {self.cache_name}")
+            with open(self.full_cache_path, "w", encoding="utf-8") as f:
+                #writing empty directory
+                json.dump({}, f)
+        else:
+            with open(self.full_cache_path, "r", encoding="utf-8") as f:
+                try:
+                    self.cache = json.load(f) # loading cache into self
+                except json.JSONDecodeError:
+                    self.cache = {} #if file is broken
 
     def check_link(self):
         # go to given link
@@ -40,32 +58,55 @@ class kleinanzeigen_monitor:
             headline = li.locator("h2 > a").first
             if headline.count() > 0:
                 headline_text = headline.get_attribute("href")
+                print(headline_text)
                 # getting the actuall ID
                 id_part = str(headline_text).split("/")[-1]
                 clean_id = id_part.split("-")[0]
-                self.seen_ids.add(clean_id)
+                offer_data = {}
+                offer_data[clean_id] = str(headline_text)
+                self.write_cache(offer_data)
             else:
                 continue
         self.write_cache()
-        
-        
-    def write_cache(self): #write cache
-        with open(self.full_cache_path, "w") as f:
-            for id in self.seen_ids:
-                f.write(f"{id}\n")
-                
-                
-                
-    def offer_observer(self):
 
-        pass
+    def write_cache(self, data):  # write cache
+        self.load_cache()
+        if not os.path.exists(self.full_cache_path):
+            return{}
+        new_id_check= data.keys() - self.cache.keys()
+        if len(new_id_check) == 0:
+            print("Already cached. Skipping")
+            pass
+        else:
+            self.cache.update(data)
+            with open(self.full_cache_path, "w", encoding="utf-8") as f:
+                json.dump(self.cache, f, indent=4, ensure_ascii=False)
+
+    def offer_observer(self):
+        print("starting Tracking of new offers")
+        
+        while True:
+            try:
+                self.check_link()
+                new_ids = self.scanned_ids - self.seen_ids
+                if len(new_ids) == 0:
+                    print("No new Listings found. Sleeping again.")
+                    wait_time = random.uniform(60, 120)
+                    time.sleep(wait_time)
+                    
+                else:
+                    print(new_ids)
+                    break
+            except Exception as e:
+                print(e)
+        
 
 
 def main():
     with sync_playwright() as p:
         link = "https://www.kleinanzeigen.de/s-preis:2000:4000/gasgas-ec-250/k0"
         mein_bot = kleinanzeigen_monitor(p, link)
-        mein_bot.check_link()
+        mein_bot.offer_observer()
 
 
 main()
