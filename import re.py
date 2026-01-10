f21b612ec2ee2110518637324f2946a280a0d389
Besix2 <myusernamethis@outlook.de>
2026-01-10 20:38:08 +0100

    added trsut score

--- import re.py
+++ import re.py
@@ -6,6 +6,8 @@ import json
 from groq import Groq
 from dotenv import load_dotenv
 import yaml
+from datetime import datetime
+import requests
 
 
 class kleinanzeigen_monitor:
@@ -32,7 +34,11 @@ class kleinanzeigen_monitor:
         #load ai
         self.ai = AiAnalyst()
         
-
+    def initial_scan(self):
+        print("Initial scan")
+        initial_scanned_offers = self.check_link() #first scan to actually fill the cache
+        self.write_cache(initial_scanned_offers)
+        
     def load_cache(self):
         #create new empty id cache
         if not os.path.exists(self.full_cache_path):
@@ -46,21 +52,23 @@ class kleinanzeigen_monitor:
                     self.cache = json.load(f) # loading cache into self
                 except json.JSONDecodeError:
                     self.cache = {} #if file is broken
+                    
     def check_link(self):
+        scanned_offers = {}
         # go to given link
         self.page.goto(self.link)
         # try to accept cookie banner
         if self.cookie_accepted:
-            return
-        print("Trying to accept cookies")
-        try:
-            self.page.get_by_test_id(
-                "gdpr-banner-accept"
-            ).click()  # accept cookie banner
-            self.cookie_accepted = True
-        except Exception as e:
-            print(e)
-        self.first_start = False
+            pass
+        else:
+            print("Trying to accept cookies")
+            try:
+                self.page.get_by_test_id(
+                    "gdpr-banner-accept"
+                ).click()  # accept cookie banner
+                self.cookie_accepted = True
+            except Exception as e:
+                print(e)
         listings_parent = self.page.locator("#srchrslt-content")
         listings = listings_parent.locator('//*[@id="srchrslt-adtable"]')
         listings_list = listings.locator("li")
@@ -74,16 +82,16 @@ class kleinanzeigen_monitor:
                 clean_id = id_part.split("-")[0]
                 offer_data = {}
                 offer_data[clean_id] = str(headline_text)
-                self.write_cache(offer_data)
+                scanned_offers.update(offer_data)
             else:
                 continue
-        self.write_cache()
+        return scanned_offers
 
     def write_cache(self, data):  # write cache
         self.load_cache()
         if not os.path.exists(self.full_cache_path):
             return{}
-        new_id_check= data.keys() - self.cache.keys()
+        new_id_check= data.keys() - self.cache.keys() #checking if id is new
         if len(new_id_check) == 0:
             print("Already cached. Skipping")
             pass
@@ -91,13 +99,13 @@ class kleinanzeigen_monitor:
             self.cache.update(data)
             with open(self.full_cache_path, "w", encoding="utf-8") as f:
                 json.dump(self.cache, f, indent=4, ensure_ascii=False)
-            self.offer_getter(data)
+
                 
-    def offer_getter(self, new_offer_data):
+    def offer_getter(self, new_offer_link):
         print("Scanning new offer...")
-        offer_link = new_offer_data
+        new_offer_link
         #offer_link = list(new_offer_data.values())[1]
-        full_link = f"https://kleinanzeigen.de{offer_link}"
+        full_link = f"https://kleinanzeigen.de{new_offer_link}"
         self.page.goto(full_link)
         self.page.get_by_test_id("gdpr-banner-accept").click()  # accept cookie banner
         details_div = self.page.locator('//*[@id="viewad-details"]/div')
@@ -108,29 +116,81 @@ class kleinanzeigen_monitor:
             #print(f"{li.inner_text()}")
         offer_description = self.page.locator('//*[@id="viewad-description-text"]')
         #print(offer_description.inner_text())
-        ai_data = offer_description.inner_text() + "\n" + offer_details_list + str(offer_link)
+        ai_data = offer_description.inner_text() + "\n" + offer_details_list + str(new_offer_link)
         Ai_result = self.ai.analyze(ai_data, self.categorie)
-        print(Ai_result)
-        test = "l"
+        #getting seller score
+        
+        badges_locator = self.page.locator(".userbadge-tag")
+        
+        all_ratings = badges_locator.all_inner_texts()
+        clean_ratings = [text.strip() for text in all_ratings]
+        #getting account creation date
+        account_creation_date = self.page.locator(".userprofile-vip-details-text" , has_text="Aktiv seit").inner_text().strip()
+        date_str = account_creation_date.replace("Aktiv seit", "").strip()
+        account_date = datetime.strptime(date_str, "%d.%m.%Y") #getting clean account creation date
+        now = datetime.now()#getting current date
+        #tranforming into year difference
+        delta = now - account_date
+        days_active = delta.days
+        years_active = days_active / 365.25
+        #score system for account creation date(youger accounts are more suspicious especially really new ones)
+        if days_active < 30:
+        # younger than 1 month
+            score = 0
+            
+        elif years_active >= 3:
+            # older than 3 years
+            score = 50
+            
+        elif years_active >= 2:
+            # older than 2 years
+            score = 30
+            
+        elif years_active >= 1:
+            # older than 1 year
+            score = 20
+            
+        else:
+            #older than one month
+            score = 10
+        if "sehr gut" in clean_ratings[0].lower():
+            score += 30
+        elif "ok" in clean_ratings[0].lower():
+            score += 10
+        
+        if len(clean_ratings) == 3:
+            score += 20
+        elif len(clean_ratings) == 2:
+            score += 10
+        print(Ai_result, score)
+        return Ai_result, score
+        
+        
+        
+        #return(Ai_result)
 
     def offer_observer(self):
         print("starting Tracking of new offers")
-        
         while True:
             try:
-                self.check_link()
-                new_ids = self.scanned_ids - self.seen_ids
+                scanned_offers = self.check_link()
+                new_ids = scanned_offers - self.cache.keys()
                 if len(new_ids) == 0:
                     print("No new Listings found. Sleeping again.")
                     wait_time = random.uniform(60, 120)
                     time.sleep(wait_time)
                     
                 else:
-                    print(new_ids)
-                    break
+                    for o in new_ids:
+                        offer_link = scanned_offers[new_ids[o]] #getting the link from the new offer
+                        Ai_result, seller_score = self.offer_getter(offer_link)
+                        
+                        
             except Exception as e:
                 print(e)
-        
+    def notifyer(self, Ai_result, seller_score):
+        pass
+        #sends message to your phone about new offer
 class AiAnalyst:
     def __init__(self):
         print("Loading config...")
